983887a4154ca1ade2937457fdf4018e438deafe
humorous-storage-7521 <rmwerner@umich.edu>
2020-07-14 11:33:06 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,22 @@
-# code here!
+require 'pry'
+class School
+    def initialize(roster)
+        @roster = {}
+    end
+    attr_accessor :roster
+    def add_student(name, grade)
+        if @roster[grade] == nil
+            @roster[grade] = []
+            @roster[grade] << name
+        else @roster[grade] << name
+        end
+    end
+    def grade(grade)
+        @roster[grade]
+    end
+    def sort
+        @roster.each do |grade, list|
+            @roster[grade] = list.sort
+        end
+    end
+end
