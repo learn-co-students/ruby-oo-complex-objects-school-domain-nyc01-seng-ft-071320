19aca356c4728bd8dcf20409ba408c1ce28d88dd
David-Kirsch <davidjkirsch@gmail.com>
2020-07-14 17:45:03 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
 # code here!
+require 'pry'
+class School
+
+    attr_accessor :roster
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+
+    def add_student(student_name, grade)
+        if(@roster[grade] == nil)
+          @roster[grade] = []
+        end
+        @roster[grade] << student_name
+    end
+
+    def grade(grade)
+        @roster[grade].map do |names|
+            names
+        end
+    end
+
+    def sort
+      @roster.each_with_object({}) do |(grade, student_name), hash|
+        hash[grade] = student_name.sort
+      end
+    end
+
+end
