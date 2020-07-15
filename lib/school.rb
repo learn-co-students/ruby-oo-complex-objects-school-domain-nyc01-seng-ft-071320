32d20b0aaee0ef190a57542f786155dd4c942858
Iuri Seara <IuriSeara@Iuri-Seara.fios-router.home>
2020-07-15 07:31:37 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,36 @@
+require 'pry'
 # code here!
+
+
+class School
+    attr_accessor :name, :roster
+    
+    def initialize (name)
+        @name = name
+        @roster = {}
+    end
+
+    def add_student(name, grade)
+
+        if  @roster[grade]
+            @roster[grade] << name
+        else 
+            @roster[grade] = []
+            @roster[grade] << name
+        end
+
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        @roster.each do |key,value|
+            @roster[key] = value.sort
+        end
+    end
+
+    # binding.pry
+end
+
